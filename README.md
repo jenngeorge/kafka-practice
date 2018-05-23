f64021360831d96302c04c12b266119a7852a49b
Jennifer Georgevich <jenngeorge@berkeley.edu>
2018-05-23 16:16:04 -0400

    Successfully stream citibike data to browser

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 work through and extend tutorial at https://scotch.io/tutorials/build-a-distributed-streaming-system-with-apache-kafka-and-python
 
+running the app locally (post-setup):
+- start zookeeper: `brew services start zookeeper`
+- start kafka: `brew services start kafka`
+- `source env/bin/activate`
+- one terminal: `python producer.py`
+- 2nd terminal: `python consumer.py`
+
+
 notes:
-- do `brew services start zookeeper` before `brew services start kafka`
+- also  pip install requests
--- consumer.py
+++ consumer.py
@@ -1,7 +1,9 @@
+import sys
 from flask import Flask, Response
 from kafka import KafkaConsumer
 #connect to Kafka server and pass the topic we want to consume
-consumer = KafkaConsumer('video', group_id='view', bootstrap_servers=['0.0.0.0:9092'])
+# consumer = KafkaConsumer('video', group_id='view', bootstrap_servers=['0.0.0.0:9092'])
+consumer = KafkaConsumer('citibike_stations', group_id='view', bootstrap_servers=['0.0.0.0:9092'])
 #Continuously listen to the connection and print messages as recieved
 app = Flask(__name__)
 
@@ -9,11 +11,13 @@ app = Flask(__name__)
 def index():
     # return a multipart response
     return Response(kafkastream(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+                    mimetype='text')
 def kafkastream():
     for msg in consumer:
-        yield (b'--frame\r\n'
-               b'Content-Type: image/png\r\n\r\n' + msg.value + b'\r\n\r\n')
+        print(msg)
+        # yield (b'--frame\r\n'
+        #        b'Content-Type: image/png\r\n\r\n' + msg.value + b'\r\n\r\n')
+        yield msg.value
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
