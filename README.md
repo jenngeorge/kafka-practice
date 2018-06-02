de7b3fb3bc87f30668ecf46f8451271f10ee7291
Jennifer Georgevich <jenngeorge@berkeley.edu>
2018-06-01 20:12:27 -0400

    Successfully get citibike data through pyspark and printed to terminal

--- README.md
+++ README.md
@@ -19,3 +19,14 @@ notes:
 pyspark notes:
 - pip install pyspark
 - run interactive pyspark with `env/bin/pyspark`
+- pip install doesnt actually include everything
+- go to http://search.maven.org/#search%7Cga%7C1%7Ca%3A%22spark-streaming-kafka-0-8-assembly_2.11%22 and download the jar
+
+
+submitting applications: https://spark.apache.org/docs/2.1.1/submitting-applications.html
+
+'if your application uses advanced sources (e.g. Kafka, Flume), then you will have to package the extra artifact they link to, along with their dependencies, in the JAR that is used to deploy the application. For example, an application using KafkaUtils will have to include spark-streaming-kafka-0-10_2.11 and all its transitive dependencies in the application JAR'
+https://spark.apache.org/docs/2.3.0/streaming-programming-guide.html#deploying-applications
+
+
+run spark application with `env/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.0 spark.py`
--- spark.py
+++ spark.py
@@ -1,3 +1,4 @@
+from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import json
@@ -9,12 +10,19 @@ ssc = StreamingContext(sc, 2)
 
 kafkaStream = KafkaUtils.createDirectStream(
     ssc,
-    ['citibike-stations'],
-    {"metadata.broker.list": ['localhost:9092']})
+    ['citibike_stations'],
+    {"metadata.broker.list": 'localhost:9092'})
 
+def printRDD(rdd):
+    print(rdd.collect())
 
-parsed = kafkaStream.map(lambda x: json.loads(x[1]))
-print(parsed)
+# parsed = kafkaStream.map(lambda x: json.loads(x[1]))
+kafkaStream.foreachRDD(printRDD)
 
 ssc.start()
 ssc.awaitTermination()
+
+
+# https://gist.github.com/rmoff/fb033086b285655ffe7f9ff0582dedbf
+# https://www.datacamp.com/courses/introduction-to-pyspark?tap_a=5644-dce66f&tap_s=210732-9d6bbf&utm_source=adwords_ppc&utm_campaignid=898687156&utm_adgroupid=48303643819&utm_device=c&utm_keyword=&utm_matchtype=b&utm_network=g&utm_adpostion=1t1&utm_creative=255798221920&utm_targetid=aud-392016246653:dsa-377762271983&utm_loc_interest_ms=&utm_loc_physical_ms=9004067&gclid=CjwKCAjwur7YBRA_EiwASXqIHBoDFRgrETDPvkc8BNHZvuajiwWGrm9UoMeicIkg9UUfhJFIMmxlUhoCIeUQAvD_BwE
+# https://www.rittmanmead.com/blog/2017/01/getting-started-with-spark-streaming-with-python-and-kafka/
